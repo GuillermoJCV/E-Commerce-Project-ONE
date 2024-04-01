a7573d5ce4bbcb7f3d358a5ab8d6ebc966bf1f5f
Mariq12 <fmlabanda@utpl.edu.ec>
2024-04-01 11:59:04 -0500

    Agregue seacrch, responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/style.css';
 import Logo from '../assets/image/logo2.png';
+import menuIcon from '../assets/icons/menu-icon.png';
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(true); // Mostrar el menú inicialmente en pantallas grandes
+
+  // Función para alternar la visibilidad del menú
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  // Ocultar el menú cuando el tamaño de la pantalla es pequeño
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setShowMenu(false);
+      } else {
+        setShowMenu(true);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Limpiar el event listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="header">
+      {/* Sección del logo */}
       <div className="logo">
-      <img src={Logo} alt="Logo" style={{ width: '100px', height: 'auto' }} /> {/* Cambiar el tamaño de la imagen*/}
+        <img src={Logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
       </div>
+
       <div className="search">
-        {/* Aquí puedes colocar tu barra de búsqueda si es necesario */}
+        {/* Barra de búsqueda */}
+        <input type="text" placeholder="Buscar productos..." />
       </div>
-      <nav className="nav">
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Productos</a></li>
-          <li><a href="#">Contacto</a></li>
-          <li><a href="#">Integrantes</a></li>
-          {/* Puedes añadir más elementos de menú aquí si es necesario */}
-        </ul>
-      </nav>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        <img src={menuIcon} alt="" />
+      </div>
+      
+      {/* Renderizar el menú si showMenu es true */}
+      {showMenu && (
+        <nav className="nav">
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Integrantes</a></li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
+
 
 export default Header;
