00dd12d147b79d9d8e82fb94627e3824b58de594
GuillermoJCV <memo92975@gmail.com>
2024-04-02 18:21:27 -0600

    Add css variables into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './App.jsx'
-=======
-import App from './App'
->>>>>>> b3ac5fca85902fa653f12de24eb1b2a680fc4bcf
+import './styles/root.css'
 
 /* Este archivo no se toca */
 
