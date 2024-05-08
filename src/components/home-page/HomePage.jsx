d7a2a3d5fddc6bfa8032be3d69248bb664738eaf
Mariq12 <fmlabanda@utpl.edu.ec>
2024-05-08 11:47:57 -0500

    Creación de las cards de productos

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -2,9 +2,249 @@ import '../../styles/homePage.css';
 
 export default function HomePage() {
 
-	return(
-		<main className="HomePage">
-			<h1>Homepage</h1>
+	return (
+		<main>
+			<section className="category">
+				<h2 className='full-width'>Sugerencias de Productos</h2>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop HP 123</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop Samsun</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+			</section>
+
+
+			<section className="category">
+				<h2 className='full-width'>Productos más vendidos</h2>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop HP 123</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop Samsun</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+			</section>
+
+			<section className="category">
+				<h2 className='full-width'>Tecnología</h2>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop HP 123</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop Samsun</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+			</section>
+
+			<section className="category">
+				<h2 className='full-width'>Cosas del hogar</h2>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop HP 123</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">Laptop Samsun</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+				<article className="card">
+					<figure className="card" alt="Producto">
+						<img
+							className="card-container--img"
+							width="100%"
+							height="60%"
+							src="https://app.pardux.com/render/img-webp/107/premiumtech/Mesa-de-tr-653157614441c"
+							alt="Producto 1"
+						/>
+						<figcaption className="card-container--info">
+							<p className="card-container--title">PC HP</p>
+						</figcaption>
+					</figure>
+				</article>
+			</section>
+
 		</main>
-	)
+	);
 }
