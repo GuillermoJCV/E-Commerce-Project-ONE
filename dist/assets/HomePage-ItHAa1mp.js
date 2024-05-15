b8e4f89dcd05783e586ca94ed217dd72c191b126
GuillermoJCV <memo92975@gmail.com>
2024-05-14 18:25:35 -0600

    Elimina el directorio dist

--- dist/assets/HomePage-ItHAa1mp.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BwIqnXIm.js";function m(){return e.jsx("main",{className:"HomePage",children:e.jsx("h1",{children:"Homepage"})})}export{m as default};
--- dist/assets/ProductsPage-DViVmO7F.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BwIqnXIm.js";function e(){return s.jsxs("main",{className:"ProductsPage",children:[s.jsx("aside",{className:"filters"}),s.jsx("div",{className:"display-products"})]})}export{e as default};
