04b7e5b04ce0b3af76f6c8b0e2d71d4abe50ae40
GuillermoJCV <memo92975@gmail.com>
2024-05-14 18:15:01 -0600

    Añade el template de las card de cada contacto

--- src/components/contacts-page/ContactsPage.jsx
+++ src/components/contacts-page/ContactsPage.jsx
@@ -6,6 +6,23 @@ export default function ContactsPage() {
 	return(
 		<main className="ContactsPage">
 			<h1>Contacts</h1>
+			<div className="ContactsPage__contacts">
+				<section className="ContactsPage__contacts__card">
+					<figure>
+						<img src="https://robohash.org/persona?set=set5" alt="perfil del contacto"/>
+						<figcaption>
+							Nombre
+						</figcaption>
+					</figure>
+					<ul>
+						<li>Skill</li>
+						<li>Skill</li>
+						<li>Skill</li>
+						<li>Skill</li>
+					</ul>
+					<a href="/contact">Link a su perfil</a>
+				</section>
+			</div>
 		</main>
 	)
 }
