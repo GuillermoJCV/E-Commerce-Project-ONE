4f68c4d44c9d295cb8c9ad22825d5e4e270f36f2
Mariq12 <fmlabanda@utpl.edu.ec>
2024-05-22 14:47:14 -0500

    Uni la function Menu en Header y es responsive

--- src/components/header-page/Header.jsx
+++ src/components/header-page/Header.jsx
@@ -1,26 +1,39 @@
+import { useRef } from "react";
 import '../../styles/header.css';
 import Logo from '../../assets/image/logo2.png';
-import searchIcon from '../../assets/icons/search-icon.png';
-import Menu from './Menu';
+import { FcSearch } from "react-icons/fc";
+
+import NavList from "./navList/NavList.jsx";
+import { toggleShowClass } from "../../utils/headerUtils/toggleShowClass.js"
+
 function Header() {
+  const menuRef = useRef(null); // Ref para el menú
+  //const loginMenuRef = useRef(null); // Ref para el menú de ingreso
+
   return (
     <header className="header">
+
       {/* Sección del logo */}
       <div className="logo">
         <img src={Logo} alt="Logo" />
       </div>
 
+      {/* Barra de búsqueda */}
       <div className="search">
-        {/* Barra de búsqueda */}
         <input type="text" placeholder="Buscar productos..." id="searchInput" />
-        {/* Contenedor del icono de búsqueda */}
         <div className="search-icon-container">
-          {/* Icono de búsqueda */}
-          <img src={searchIcon} alt="Search" className="search-icon" />
+          <FcSearch className="search-icon" />
         </div>
       </div>
 
-      <Menu/>
+      {/* Renderizar el menú */}
+      <div>
+        <div className="menu-icon" onClick={() => toggleShowClass(menuRef)}></div>
+        <nav ref={menuRef} className="nav tablet-nav">
+          <NavList />
+        </nav>
+      </div>
+      
     </header>
   );
 }
