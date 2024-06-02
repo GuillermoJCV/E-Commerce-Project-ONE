f99952a457282db90fd0f3230458b43f12e31143
GuillermoJCV <memo92975@gmail.com>
2024-06-02 11:27:23 -0600

    Cambia el nombre de la funcion a singular porque solamente trae una variable

--- src/utils/getRootCSSVariables/getVariables.js
+++ src/utils/getRootCSSVariables/getVariables.js
@@ -1,16 +1,16 @@
 const html = document.getElementsByTagName("html")[0]
 
-function getCssVariables(cssVarName = "") {
+function getCssVariable(cssVarName = "") {
 	if(typeof cssVarName !== "string" || !cssVarName) throw new TypeError("1st argument must be an string and can't be empty")
 
 	const computedStyles = globalThis.getComputedStyle(html)
 	const cssVarValue = computedStyles.getPropertyValue(cssVarName)
-	
+
 	if(!cssVarValue) throw new ReferenceError(`${cssVarValue} not found, maybe double hyphen missed (--${cssVarName})`)
 	return cssVarValue
 }
 
 
 export {
-	getCssVariables
+	getCssVariable
 }
