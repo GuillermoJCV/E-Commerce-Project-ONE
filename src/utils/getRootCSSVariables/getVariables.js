1899829875e883e5057e563f789f427bb00293bc
GuillermoJCV <memo92975@gmail.com>
2024-06-02 11:21:47 -0600

    Maneja los errores por si se ingresa un valor invalido

--- src/utils/getRootCSSVariables/getVariables.js
+++ src/utils/getRootCSSVariables/getVariables.js
@@ -1,9 +1,11 @@
 const html = document.getElementsByTagName("html")[0]
 
 function getCssVariables(cssVarName = "") {
-	if(typeof cssVarName !== "string") throw new TypeError("1st argument must be an string and can't be empty")
+	if(typeof cssVarName !== "string" || !cssVarName) throw new TypeError("1st argument must be an string and can't be empty")
+
 	const computedStyles = globalThis.getComputedStyle(html)
 	const cssVarValue = computedStyles.getPropertyValue(cssVarName)
+	
 	if(!cssVarValue) throw new ReferenceError(`${cssVarValue} not found, maybe double hyphen missed (--${cssVarName})`)
 	return cssVarValue
 }
