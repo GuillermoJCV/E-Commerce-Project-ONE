9df56fd566c0d43df543e2a81317496a0efd0ab1
GuillermoJCV <memo92975@gmail.com>
2024-06-02 14:50:39 -0600

    Cambia la funcion de getCssVariable para que no tenga problemas con el testing

--- src/utils/getRootCSSVariables/getVariables.js
+++ src/utils/getRootCSSVariables/getVariables.js
@@ -1,13 +1,15 @@
-const html = document.getElementsByTagName("html")[0]
+const html = window.document.getElementsByTagName("html")[0]
 
 function getCssVariable(cssVarName = "") {
-	if(typeof cssVarName !== "string" || !cssVarName) throw new TypeError("1st argument must be an string and can't be empty")
-
-	const computedStyles = globalThis.getComputedStyle(html)
-	const cssVarValue = computedStyles.getPropertyValue(cssVarName)
-
-	if(!cssVarValue) throw new ReferenceError(`${cssVarValue} not found, maybe double hyphen missed (--${cssVarName})`)
-	return cssVarValue
+	if(window.getComputedStyle(html).getPropertyValue(cssVarName)){
+		if(typeof cssVarName !== "string" || !cssVarName) throw new TypeError("1st argument must be an string and can't be empty")
+	
+		const computedStyles = window.getComputedStyle(html)
+		const cssVarValue = computedStyles.getPropertyValue(cssVarName)
+	
+		if(!cssVarValue) throw new ReferenceError(`${cssVarValue} not found, maybe double hyphen missed (--${cssVarName})`)
+		return cssVarValue
+	}
 }
 
 
